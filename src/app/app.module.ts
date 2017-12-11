bd7dfa9a4b67ff2a78b8cb6dcfc2bfa2e5b787e1
Gyles Batara <gyles.batara@live.com>
2017-12-11 09:29:29 -0800

    Almost done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PipeNamePipe } from './pipe-name.pipe';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PipeNamePipe,
+    AboutComponent,
+    MarketplaceComponent
   ],
   imports: [
     BrowserModule,
